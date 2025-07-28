d3b46f1852454ba6f79562caf9aa9fef1504f3ba
snonong <snong1m1@gmail.com>
2025-07-29 01:51:00 +0900

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask("")
+app = Flask('')
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "봇이 실행 중입니다!"
+    return "I'm alive!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
